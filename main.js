547d078a611a17d73ef474e67f61898af3ba5b7e
William Harkcom <w.harkcom@gmail.com>
2014-01-05 05:26:55 +0000

    Added extra sections, fwd/back support

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 
 	//Menu Links
 	$('.menuOption').click(function(){
-		$('.menuOption').removeClass('selected');
-		$(this).addClass('selected');
+		//$('.menuOption').removeClass('selected');
+		//$(this).addClass('selected');
 
-		$('.resumeSection').hide();
-		$('#'+$(this).attr('value')).show();
+		href = $(this).attr('value');
+		window.history.pushState(null, null, '?'+href);
+		loadContent(href);
 	});
 
 	//Resume Section
@@ -24,5 +25,17 @@ $(document).ready(function() {
 	});
 });
 
+//Show correct resume section
+function loadContent(href) {
+	$('.menuOption').removeClass('selected');
+	$('#'+href+'Menu').addClass('selected');
 
+	$('.resumeSection').hide();
+	$('#'+href).show();
+}
 
+//Return to right location using back button
+$(window).bind("popstate", function() {
+    link = location.href.replace(/^.*[\\/]/, ""); // get filename only
+    loadContent(link.substring(1));
+});
